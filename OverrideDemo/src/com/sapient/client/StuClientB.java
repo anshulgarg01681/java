43bdf149bf9278d46157c7bb0d8c463ba171b3db
agar41 <agar41@NOILAB5309.sapient.com>
2016-06-28 14:09:45 +0530

    anshul committed

---
+++ OverrideDemo/src/com/sapient/client/StuClientB.java
@@ -0,0 +1,19 @@
+package com.sapient.client;
+
+import com.sapient.beans.Student;
+
+public class StuClientB {
+
+	public static void main(String[] args) {
+		Student stu = new Student(1001,"ram", 89);
+		Student stu2 = new Student(1001,"ram", 89);
+		
+		stu = null;
+		stu2 = null;
+		System.gc();
+
+		System.out.println("line 2");
+		System.out.println("line 3");
+	}
+
+}
